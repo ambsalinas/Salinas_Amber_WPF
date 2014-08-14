4af4348a302298b2d86bd22d3d93c5314fd6b0d5
Amber <ambsalinas@gmail.com>
2014-08-14 19:50:37 -0400

    Added required info at the top of the js sheet, along with a commented description of why the following code is relevant to my industry.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,1 +1,3 @@
 // Amber Salinas -- 14 August 2014 -- Expressions (Industry)
+
+// In the Army (my current profession), Soldiers are required to pass certain height, weight, and body-fat percentage standards. 
