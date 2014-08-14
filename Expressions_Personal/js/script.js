05dbd5edebdc81e94a599fb82b90309341fddd3c
Amber <ambsalinas@gmail.com>
2014-08-14 18:54:32 -0400

    Added starting alerts for description of task and instructions on how to complete it.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,1 +1,5 @@
-// JavaScript Document
+// Amber Salinas -- 14 Aug 2014 -- Expressions (Personal)
+
+alert("Find the square feet of carpet required to carpet the basement floor."); // The task
+
+alert("The basement is an irregular polygon, so we must divide it into two different rectangles, find the area of each, and add those numbers together."); // The instructions
