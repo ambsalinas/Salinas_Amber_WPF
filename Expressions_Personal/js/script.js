5fd4425220b71385f1b5243cf1e8c015c2feebc4
Amber <ambsalinas@gmail.com>
2014-08-14 19:04:10 -0400

    Created a variable for the area of the first rectangle.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,8 +4,10 @@ alert("Find the square feet of carpet required to carpet the basement floor."); 
 
 alert("The basement is an irregular polygon, so we must divide it into two different rectangles, find the area of each, and add those numbers together."); // The instructions
 
-var length1 = prompt("What is the length of the first rectangle?"); 
-console.log("The length of the first rectangle is " + length1 + " feet."); 
+var length1 = prompt("What is the length of the first rectangle?"); // finding the length of the larger rectangle
+console.log("The length of the first rectangle is " + length1 + " feet."); // printing the user's input to the console
 
-var height1 = prompt("What is the height of the first rectangle?"); 
-console.log("The height of the first rectangle is " + height1 + " feet."); 
+var height1 = prompt("What is the height of the first rectangle?"); // finding the height of the larger rectangle
+console.log("The height of the first rectangle is " + height1 + " feet."); // printing the user's input to the console
+
+var area1 = length1 * height1; // finding the area of the larger rectangle by multiplying length by height. 
