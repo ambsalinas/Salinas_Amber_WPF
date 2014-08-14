577dc4263fbc25aa750994351e52a4a1d6055c89
Amber <ambsalinas@gmail.com>
2014-08-14 18:59:52 -0400

    Added prompt for height of first triangle

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -4,6 +4,8 @@ alert("Find the square feet of carpet required to carpet the basement floor."); 
 
 alert("The basement is an irregular polygon, so we must divide it into two different rectangles, find the area of each, and add those numbers together."); // The instructions
 
-var length1 = prompt("What is the length of the larger rectangle?"); 
+var length1 = prompt("What is the length of the first rectangle?"); 
 
 console.log("The length of the first rectangle is " + length1 + " feet."); 
+
+var height1 = prompt("What is the height of the first rectangle?"); 
