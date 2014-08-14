1f311b295676bbad1e7f074bab066222794473f4
Amber <ambsalinas@gmail.com>
2014-08-14 19:00:43 -0400

    Printed height of first rectangle to console

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -5,7 +5,7 @@ alert("Find the square feet of carpet required to carpet the basement floor."); 
 alert("The basement is an irregular polygon, so we must divide it into two different rectangles, find the area of each, and add those numbers together."); // The instructions
 
 var length1 = prompt("What is the length of the first rectangle?"); 
-
 console.log("The length of the first rectangle is " + length1 + " feet."); 
 
 var height1 = prompt("What is the height of the first rectangle?"); 
+console.log("The height of the first rectangle is " + height1 + " feet."); 
