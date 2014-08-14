08b2844fe74622f8717f010e3af3a2d213c5ab98
Amber <ambsalinas@gmail.com>
2014-08-14 18:56:14 -0400

    Added prompt for length of first rectangle.

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,3 +3,5 @@
 alert("Find the square feet of carpet required to carpet the basement floor."); // The task
 
 alert("The basement is an irregular polygon, so we must divide it into two different rectangles, find the area of each, and add those numbers together."); // The instructions
+
+var length1 = prompt("What is the length of the larger rectangle?"); 
