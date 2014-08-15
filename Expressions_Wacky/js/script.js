db61d555112d0b4e080dfc975cc5af93759994ad
Amber <ambsalinas@gmail.com>
2014-08-14 21:03:33 -0400

    created variables to determine additional calories needed for user's level of activity.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -8,6 +8,10 @@ var weight = prompt("Your weight in pounds:"); // obtain user's weight
 var basicNeedsMale = weight * 11; 
 var basicNeedsFemale = weight * 10; // find basic needs without any activity for both males and females 
 
+console.log("Your basic caloric needs without any physical activity is " + basicNeedsMale + " calories if you are male, and " + basicNeedsFemale + " calories if you are female."); 
+
 var activity = prompt("What is your activity level? \n20%: Sedentary, a lot of sitting, driving, or sleeping. \n30%:Light activity <2 hours a day. \n40%:Moderate exercise with little time spent sitting. \n50%:Very active in sports or exercise, or involved in an intensive-labor job such as construction."); // find out how active the user is
-var activeNeedsMale = basicNeedsMale * activity; 
-var activeNeedsFemale = basicNeedsFemale * activity; // find active caloric needs for men and women
+var activeNeedsMale = basicNeedsMale * (activity *.01); 
+var activeNeedsFemale = basicNeedsFemale * (activity *.01); // find active caloric needs for men and women. multiply activity by .01 to get the percentage.
+
+console.log("Your additional caloric needs with your level of physical activity are " + activeNeedsMale + " calories if you are male, and " + activeNeedsFemale + " calories if you are female."); 
