73f811bc57711992e185123c04365733d3864327
Amber <ambsalinas@gmail.com>
2014-08-14 20:49:59 -0400

    Set up first alert, and the first variables to determine basic caloric needs for men and women (without activity).

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,1 +1,9 @@
 // Amber Salinas -- 14 Aug 2014 -- Expressions (Wacky)
+
+// Calculate your Basal Caloric Needs (how many calories you need just to survive)
+
+alert("Let's calculate your Basal Caloric Needs!"); // an alert to let users know what we are doing
+
+var weight = prompt("Your weight in pounds:"); // obtain user's weight
+var basicNeedsMale = weight * 11; 
+var basicNeedsFemale = weight * 10; // find basic needs without any activity for both males and females 
