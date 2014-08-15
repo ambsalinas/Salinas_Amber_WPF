191856db7e21d3b4273de87f6b3f9aa03601223a
Amber <ambsalinas@gmail.com>
2014-08-14 20:56:27 -0400

    Prompt user for level of activity, create variables for male/female active caloric needs.

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -7,3 +7,7 @@ alert("Let's calculate your Basal Caloric Needs!"); // an alert to let users kno
 var weight = prompt("Your weight in pounds:"); // obtain user's weight
 var basicNeedsMale = weight * 11; 
 var basicNeedsFemale = weight * 10; // find basic needs without any activity for both males and females 
+
+var activity = prompt("What is your activity level? \n20%: Sedentary, a lot of sitting, driving, or sleeping. \n30%:Light activity <2 hours a day. \n40%:Moderate exercise with little time spent sitting. \n50%:Very active in sports or exercise, or involved in an intensive-labor job such as construction."); // find out how active the user is
+var activeNeedsMale = basicNeedsMale * activity; 
+var activeNeedsFemale = basicNeedsFemale * activity; // find active caloric needs for men and women
