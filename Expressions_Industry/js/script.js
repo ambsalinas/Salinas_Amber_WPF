85a45051f1cf7d586b15c86fb86cf5103672fa0e
Amber <ambsalinas@gmail.com>
2014-08-14 20:06:30 -0400

    Prompt the user for their weight in pounds and print result to the console.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -3,3 +3,6 @@
 // In the Army (my current profession), Soldiers are required to pass certain height, weight, and body-fat percentage standards. 
 
 alert("Calculate your Body Mass Index."); // create an alert for what the user is about to do
+
+var weight = prompt("What is your weight in pounds?"); // obtain user's weight in pounds
+console.log("Weight: " + weight + "lbs."); // print the result to the console.
