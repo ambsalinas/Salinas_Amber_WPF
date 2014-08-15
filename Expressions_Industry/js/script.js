14a4a570fdb793f0f766a8ac18c96629d1c3672a
Amber <ambsalinas@gmail.com>
2014-08-14 20:02:24 -0400

    Created an alert for calculating your BMI.

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,3 +1,5 @@
 // Amber Salinas -- 14 August 2014 -- Expressions (Industry)
 
 // In the Army (my current profession), Soldiers are required to pass certain height, weight, and body-fat percentage standards. 
+
+alert("Calculate your Body Mass Index."); // create an alert for what the user is about to do
