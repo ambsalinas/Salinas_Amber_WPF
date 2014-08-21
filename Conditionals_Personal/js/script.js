8fa4a5e6e787baf0d7f3f4896fd9edef4a0b39cb
Amber <ambsalinas@gmail.com>
2014-08-21 19:05:02 -0400

    Fixed an error on line 7.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -4,7 +4,7 @@
 
 var totalCost = prompt("What is the total cost to carpet the basement?"); // prompt the user to determine what their total cost is.
 
-if (totalCost = ""){
+if (totalCost == ""){
 	console.log("We need the total before we can go any further!"); // make sure the user input something!
 }else{
 	var budget = prompt("What is your budget for this month?");
