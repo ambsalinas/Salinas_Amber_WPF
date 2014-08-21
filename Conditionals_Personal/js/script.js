b197e35a45a34ac2937f80f77d95c9aa26a89a55
Amber <ambsalinas@gmail.com>
2014-08-21 19:19:57 -0400

    created a prompt to determine the user's budget, and then print to the console.

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -7,5 +7,7 @@ var totalCost = prompt("What is the total cost to carpet the basement?"); // pro
 if (totalCost == ""){
 	var totalCost = prompt("We need the total before we can go any further!"); // make sure the user inputs something!
 }else{
-	var budget = prompt("What is your budget for this month?");
+	var budget = prompt("What is your budget for this month?"); // once the user has input a total, let's find out what their monthly budget is.
 }
+console.log("You have $" + budget + " to spend this month."); // print their budget to the console
+
