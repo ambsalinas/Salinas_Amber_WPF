dfce0d1ae0e779614a6702663427463cb3cdfb87
Amber <ambsalinas@gmail.com>
2014-08-21 22:35:28 -0400

    Creating the constants for the formula, then putting it all together!

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -4,5 +4,9 @@
 
 var time = prompt("How long are you falling?"); // get the time from the user
 // the formula is d = 1/2 * 9.8t^2, so we need to define variables for the rest of the equation. 
-const grav = .5; 
+const grav = .5; // we use .5 instead of 1/2 
 const a = 9.8; // the constant for gravity
+
+var timeGrav = .5 * a; // part one of the equation.  
+var gravTime = time * time; // part two
+var distance = timeGrav * gravTime; // final total
