11a8c92a24e89a55ea703fa09c80679ffd5a432d
Amber <ambsalinas@gmail.com>
2014-08-26 19:01:00 -0400

    Created the variables and function to calculate how many bee stings would be fatal based on animal's weight.

--- Functions_Worksheet/js/script.js
+++ Functions_Worksheet/js/script.js
@@ -12,3 +12,13 @@ function calculateCirc(pi, radius){ // declare the function
 	return circ // return the result of the function
 }
 
+// Stung! 
+
+var weight = 250; // the animal's weight
+var death = calcDeath(weight); // give the function a variable and its parameters
+console.log("It would take " + death + " bee stings to kill an animal weighing " + weight + "lbs.");
+
+function calcDeath(weight){ // declare the function
+	var death = weight * 8.666666667; // multiply animal's weight by number of bee stings/pound it takes to kill 
+	return death // return the result
+}
