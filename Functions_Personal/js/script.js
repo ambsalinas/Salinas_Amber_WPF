b1feec67a9ed965ade98fd58f36e199e09816123
Amber <ambsalinas@gmail.com>
2014-08-27 19:13:34 -0400

    Used the Math.random function for the computer's choices.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -3,3 +3,9 @@
 // My husband loves to use "Rock, Paper, Scissors" to determine who does the least enjoyed chores around the house. I decided to create a "Rock, Paper, Scissors" game in his honor! I made it so that the user can play against the computer.
 
 var myChoice = prompt("Rock, Paper, or Scissors?"); // here we make a prompt for the user to make their choice
+
+// I had to do a little bit of research for this next part. I knew I wanted the computer to choose a random number so that I could make a certain range equal to "rock," a range equal to "paper," etc. but I wasn't sure how to do it. Google led me to w3schools.com, which revealed the Math.random function.
+
+var compChoice = Math.random(); // we create a variable for the computer to pick a random number. 
+
+// According to w3schools, the random number will be between 0 and 1. I need to create if/else if/else statements that will define what results in rock, paper, and scissors.
