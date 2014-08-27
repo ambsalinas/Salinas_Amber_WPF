2c4868a494e5f25de419861cc5e77864665d6965
Amber <ambsalinas@gmail.com>
2014-08-27 18:23:52 -0400

    Set up the basis for the function to determine how many times an athlete's heart might beat in one day at 40 beats-per-minute.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,24 +1,5 @@
-// Amber Salinas -- 26 Aug 2014 -- Functions Worksheet
+// Amber Salinas -- 27 August 2014 -- Functions_Wacky
 
-// Calculate the Circumference of a Circle
-var pi = 3.14; // pi
-var radius = 15; // the radius
-var circ = calculateCirc(pi, radius); // give the function a variable
-console.log("The circumference of the circle is " + circ + "."); // print the result
+// A well-trained athlete could have a resting heart rate of 40 beats-per-minute or less. Let's create a simple function to determine how many times an athlete's heart may beat in 1 day at 40bpm.
 
-
-function calculateCirc(pi, radius){ // declare the function
-	var circ = (2 * pi) * radius; // the formula for circumference is 2 * pi * r. 
-	return circ // return the result of the function
-}
-
-// Stung! 
-
-var weight = 250; // the animal's weight
-var death = calcDeath(weight); // give the function a variable and its parameters
-console.log("It would take " + death + " bee stings to kill an animal weighing " + weight + "lbs.");
-
-function calcDeath(weight){ // declare the function
-	var death = weight * 8.666666667; // multiply animal's weight by number of bee stings/pound it takes to kill 
-	return death // return the result
-}
+var beatsPerDay = calcPerDay(40, 1440); // call the results of the function inside the variable beatsPerDay, using predefined parameters of 40, for 40 bpm, and 1440, for 1,440 minutes in one day.
