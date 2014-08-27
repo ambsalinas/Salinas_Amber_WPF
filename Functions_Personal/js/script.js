daa927097b70d2e03f199803e03b736d6b280cd7
Amber <ambsalinas@gmail.com>
2014-08-27 19:05:23 -0400

    Created the basis for a "Rock, Paper, Scissors" script.

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,24 +1,5 @@
-// Amber Salinas -- 26 Aug 2014 -- Functions Worksheet
+// Amber Salinas -- 27 August 2014 -- Functions_Personal
 
-// Calculate the Circumference of a Circle
-var pi = 3.14; // pi
-var radius = 15; // the radius
-var circ = calculateCirc(pi, radius); // give the function a variable
-console.log("The circumference of the circle is " + circ + "."); // print the result
+// My husband loves to use "Rock, Paper, Scissors" to determine who does the least enjoyed chores around the house. I decided to create a "Rock, Paper, Scissors" game in his honor! I made it so that the user can play against the computer.
 
-
-function calculateCirc(pi, radius){ // declare the function
-	var circ = (2 * pi) * radius; // the formula for circumference is 2 * pi * r. 
-	return circ // return the result of the function
-}
-
-// Stung! 
-
-var weight = 250; // the animal's weight
-var death = calcDeath(weight); // give the function a variable and its parameters
-console.log("It would take " + death + " bee stings to kill an animal weighing " + weight + "lbs.");
-
-function calcDeath(weight){ // declare the function
-	var death = weight * 8.666666667; // multiply animal's weight by number of bee stings/pound it takes to kill 
-	return death // return the result
-}
+var myChoice = prompt("Rock, Paper, or Scissors?"); // here we make a prompt for the user to make their choice
--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,13 +3,13 @@
 // A well-trained athlete could have a resting heart rate of 40 beats-per-minute or less. Let's create a simple function to determine how many times an athlete's heart may beat in 1 day at 40bpm.
 
 var beatsPerDay = calcPerDay(40, 1440); // call the results of the function inside the variable beatsPerDay, using predefined parameters of 40, for 40 bpm, and 1440, for 1,440 minutes in one day.
-
 console.log("A well-trained athlete's heart might beat at 40bpm, which is " + beatsPerDay + " beats per day!"); // print the results
 
 function calcPerDay(bpm, minutes){ // define the function and its parameters
-    var beatsPerDay = bpm * minutes; // the code to be executed
-    return beatsPerDay; // return the result!
+var beatsPerDay = bpm * minutes; // the code to be executed
+return beatsPerDay; // return the result!
 }
+
 // Now let's figure out how many times an athlete's heart might beat in x number of days (input by user).
 
 var days = prompt("Enter number of days."); // prompt the user for the number of days they'd like to use
@@ -18,6 +18,6 @@ var beats = calcDays(minutes, 40); // assign the results of the function to a va
 console.log("An athlete's heart will beat " + beats + " times in " + days + " days at 40bpm.");
 
 function calcDays(minutes, bpm){
-    var beats = minutes * bpm; // the code to be executed
-    return beats; // return the result!
+var beats = minutes * bpm; // the code to be executed
+return beats; // return the result!
 }
