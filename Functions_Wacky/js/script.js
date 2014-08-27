9cc599ccd9c5b337ec9c115ac3da7758daf0fbaf
Amber <ambsalinas@gmail.com>
2014-08-27 18:25:02 -0400

    Defined the function and its parameters. Returned results of the function's code, and printed results to the console.

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,3 +3,10 @@
 // A well-trained athlete could have a resting heart rate of 40 beats-per-minute or less. Let's create a simple function to determine how many times an athlete's heart may beat in 1 day at 40bpm.
 
 var beatsPerDay = calcPerDay(40, 1440); // call the results of the function inside the variable beatsPerDay, using predefined parameters of 40, for 40 bpm, and 1440, for 1,440 minutes in one day.
+
+console.log("A well-trained athlete's heart might beat at 40bpm, which is " + beatsPerDay + " beats per day!"); // print the results
+
+function calcPerDay(bpm, minutes){ // define the function and its parameters
+    var beatsPerDay = bpm * minutes; // the code to be executed
+    return beatsPerDay; // return the result!
+}
