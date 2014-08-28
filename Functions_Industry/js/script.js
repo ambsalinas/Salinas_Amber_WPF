e7f2cc14327aeeffab32b6d397d129cd0173c0c3
Amber <ambsalinas@gmail.com>
2014-08-28 16:50:40 -0400

    Created the initial prompt to get the necessary information from the user.

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -1,24 +1,5 @@
-// Amber Salinas -- 26 Aug 2014 -- Functions Worksheet
+// Amber Salinas -- 28 August 2014 -- Functions_Industry
 
-// Calculate the Circumference of a Circle
-var pi = 3.14; // pi
-var radius = 15; // the radius
-var circ = calculateCirc(pi, radius); // give the function a variable
-console.log("The circumference of the circle is " + circ + "."); // print the result
+// Every 6 months my Unit holds a firing range so that all Soldiers can qualify with their weapons. There is a lot of planning that goes into a range, and one of the most important factors to consider is how much ammunition will be needed to ensure that there is enough for everyone. Here is a script that could help the Range Noncommissioned-Officer In Charge (NCOIC) determine how much ammo is needed.
 
-
-function calculateCirc(pi, radius){ // declare the function
-	var circ = (2 * pi) * radius; // the formula for circumference is 2 * pi * r. 
-	return circ // return the result of the function
-}
-
-// Stung! 
-
-var weight = 250; // the animal's weight
-var death = calcDeath(weight); // give the function a variable and its parameters
-console.log("It would take " + death + " bee stings to kill an animal weighing " + weight + "lbs.");
-
-function calcDeath(weight){ // declare the function
-	var death = weight * 8.666666667; // multiply animal's weight by number of bee stings/pound it takes to kill 
-	return death // return the result
-}
+var soldiers = prompt("How many Soldiers will be attending the range?"); // prompt the NCOIC to determine how many total Soldiers will be firing
